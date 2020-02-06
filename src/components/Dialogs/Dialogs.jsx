273c0aab65dbba6c6226cf5f653b8845573f9ed7
ihorock <onyshchukihor11@gmail.com>
2020-02-06 21:30:50 +0200

    added dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
-import './Dialogs.module.css';
+import s from './Dialogs.module.css';
+import {NavLink} from 'react-router-dom';
 
 const Dialogs = props => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/1">Edd</NavLink>
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to="/dialogs/2">Ihor</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/3">Nickolas</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/4">Marty</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/5"> Emmet</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    HI!
+                </div>
+                <div className={s.message}>
+                    Hello!
+                </div>
+                <div className={s.message}>
+                    What's up guys!
+                </div>
+            </div>
         </div>
     );
 };
