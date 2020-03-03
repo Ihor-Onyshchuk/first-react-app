75208a3f0fb9ee4451039744646428414d3da031
ihorock <onyshchukihor11@gmail.com>
2020-03-03 16:18:23 +0200

    Updates

---
+++ precache-manifest.93a46853c6f8a6cdd97b0a790cb360a6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3041f947e9710d03f3fdc997420d4129",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "23505fc63600cb79e790",
+    "url": "/first-react-app/static/css/3.cfdd863b.chunk.css"
+  },
+  {
+    "revision": "0ea36680565607b83f39",
+    "url": "/first-react-app/static/css/4.cd2222ae.chunk.css"
+  },
+  {
+    "revision": "15c914abe9985ba84892",
+    "url": "/first-react-app/static/css/main.110d8555.chunk.css"
+  },
+  {
+    "revision": "751d0f12ddd1c96fb4de",
+    "url": "/first-react-app/static/js/2.692e40fc.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/first-react-app/static/js/2.692e40fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23505fc63600cb79e790",
+    "url": "/first-react-app/static/js/3.9a2acf63.chunk.js"
+  },
+  {
+    "revision": "0ea36680565607b83f39",
+    "url": "/first-react-app/static/js/4.b24b7384.chunk.js"
+  },
+  {
+    "revision": "15c914abe9985ba84892",
+    "url": "/first-react-app/static/js/main.414b11b2.chunk.js"
+  },
+  {
+    "revision": "7e2dbbfaa0c5167a6904",
+    "url": "/first-react-app/static/js/runtime-main.9e630566.js"
+  },
+  {
+    "revision": "47b6080fc6f3721dd9bc7be8895215f8",
+    "url": "/first-react-app/static/media/loader2.47b6080f.svg"
+  }
+]);
